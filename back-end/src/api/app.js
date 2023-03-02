a17f7512a8cbfec16861b178b6c6ca6deb823cac
Brenno-Lucas <brennolucas8@gmail.com>
2023-03-01 21:00:41 -0300

    Correção requisito 04

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 
 const LoginRouter = require('../routes/login.router');
 
+var corsOptions = {
+  origin: 'http://localhost:3000',
+  methods: ['POST', 'GET', 'PUT', 'DELETE'],
+};
+
 const app = express();
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.get('/coffee', (_req, res) => res.status(418).end());
