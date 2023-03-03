3b6737d0440589625138494a896e35a841300f93
juliana.reitz <juliana.mreitz@gmail.com>
2023-03-03 15:39:34 -0300

    feat - getOne para target add to cart

--- back-end/src/routes/customer.router.js
+++ back-end/src/routes/customer.router.js
@@ -5,4 +5,7 @@ const router = Router();
 
 router.get('/products', CustomerController.getAll);
 
+router.get('/products', CustomerController.getOne);
+
+
 module.exports = router;
--- back-end/src/service/customer.service.js
+++ back-end/src/service/customer.service.js
@@ -2,4 +2,11 @@ const { Product } = require('../database/models');
 
 const getAll = async () => Product.findAll();
 
-module.exports = { getAll };
+// pega um produto apenas (conectar com a função de adicionar ao carrinho)
+const getOne = async (id) => Product.findOne({ where: { id }});
+
+
+module.exports = { 
+  getAll,
+  getOne,
+ };
