89cf1c3b44cd6efd4df06e57c0f96a0c1a203cb9
juliana.reitz <juliana.mreitz@gmail.com>
2023-03-03 18:16:01 -0300

    feat - maybe login backend tests

---
+++ back-end/src/tests/unit/mocks/user.test.js
@@ -0,0 +1,13 @@
+export const loginResponse = {
+  'id': 3,
+  'name': 'Cliente Zé Birita',
+  'email': 'zebirita@email.com',
+  'role': 'customer'
+};
+
+export const loginPost = {
+  'email': 'zebirita@email.com',
+  'password': '$#zebirita#$'
+};
+
+export const notFound = 'Not found';
