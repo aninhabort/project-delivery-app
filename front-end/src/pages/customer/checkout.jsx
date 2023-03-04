ce77ca73a27afb8ebfe55048575fc246a9871e1b
willian-macedo <willian.macedo@gmail.com>
2023-03-04 18:58:52 -0300

    working on checkout page [17]

--- front-end/src/pages/customer/checkout.jsx
+++ front-end/src/pages/customer/checkout.jsx
@@ -1,7 +1,75 @@
+import { useSelector } from 'react-redux';
+import { selectCart } from '../../redux/cart';
 import Navbar from '../../components/Navbar';
+// import ProductCard from '../../components/productCard';
 
 export default function Checkout() {
+  const cart = useSelector(selectCart);
+  // const id = 0;
   return (
-    <Navbar />
+    <main>
+      <Navbar />
+      <div>
+        {
+          cart.map((product, i) => (
+            <div key={ i }>
+              <div
+                key={ i }
+                data-testid={ `customer_checkout__element-order-table-item-number-${i}` }
+              >
+                { i }
+              </div>
+              <div
+                key={ i }
+                data-testid={ `customer_checkout__element-order-table-name-${i}` }
+              >
+                { product.name }
+              </div>
+              <div
+                key={ i }
+                data-testid={ `customer_checkout__element-order-table-quantity-${i}` }
+              >
+                { product.quantity }
+              </div>
+              <div
+                key={ i }
+                data-testid={ `customer_checkout__element-order-table-unit-price-${i}` }
+              >
+                { product.price }
+              </div>
+              <div
+                key={ i }
+                data-testid={ `customer_checkout__element-order-table-sub-total-${i}` }
+              >
+                { product.quantity * product.price }
+              </div>
+              <div
+                key={ i }
+                data-testid={ `customer_checkout__element-order-table-remove-${i}` }
+              >
+                [REMOVE]
+              </div>
+            </div>
+
+            // <ProductCard
+            //   key={ product.id }
+            //   product={ { ...product } }
+            // />
+          ))
+        }
+      </div>
+      {/* <p data-testid={ `customer_checkout__element-order-table-unit-price-${id}` } /> */}
+      {/* <p data-testid={ `customer_checkout__element-order-table-sub-total-${id}` } /> */}
+      {/* <p data-testid={ `customer_checkout__element-order-table-remove-${id}` } /> */}
+      <p data-testid="customer_checkout__element-order-total-price" />
+      <select data-testid="customer_checkout__select-seller">
+        <option value="seller">seller</option>
+      </select>
+      <input type="text" data-testid="customer_checkout__input-address" />
+      <input type="text" data-testid="customer_checkout__input-address-number" />
+      <button type="button" data-testid="customer_checkout__button-submit-order">
+        Finalizar
+      </button>
+    </main>
   );
 }
