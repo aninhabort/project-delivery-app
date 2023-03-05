2034381e0b28ab264b090effe182d2a2a0f748fe
willian-macedo <willian.macedo@gmail.com>
2023-03-05 12:09:39 -0300

    working on remove item feat.

--- front-end/src/pages/customer/checkout.jsx
+++ front-end/src/pages/customer/checkout.jsx
@@ -6,6 +6,15 @@ export default function Checkout() {
   const cart = useSelector(selectCart).filter((product) => (product.quantity > 0));
   const totalValue = useSelector(selectTotalValue);
 
+  const remove = (i) => {
+    const target = Number(i.target.attributes[1].nodeValue.split('-').pop());
+    // console.log('LOG target', target);
+    console.log('LOG cart <', cart);
+    console.log('prod.id !==', target);
+    const newCart = cart.filter((prod) => prod.id !== target);
+    console.log('LOG cart >', newCart);
+  };
+
   return (
     <main>
       <Navbar />
@@ -15,7 +24,7 @@ export default function Checkout() {
             // componente? {
             <div key={ i }>
               <div
-                key={ i }
+                // key={ i }
                 data-testid={
                   `customer_checkout__element-order-table-item-number-${i}`
                 }
@@ -23,19 +32,19 @@ export default function Checkout() {
                 { i + 1 }
               </div>
               <div
-                key={ i }
+                // key={ i }
                 data-testid={ `customer_checkout__element-order-table-name-${i}` }
               >
                 { product.name }
               </div>
               <div
-                key={ i }
+                // key={ i }
                 data-testid={ `customer_checkout__element-order-table-quantity-${i}` }
               >
                 { product.quantity }
               </div>
               <div
-                key={ i }
+                // key={ i }
                 data-testid={
                   `customer_checkout__element-order-table-unit-price-${i}`
                 }
@@ -44,7 +53,7 @@ export default function Checkout() {
                 { product.price.split('.').join(',') }
               </div>
               <div
-                key={ i }
+                // key={ i }
                 data-testid={
                   `customer_checkout__element-order-table-sub-total-${i}`
                 }
@@ -52,12 +61,14 @@ export default function Checkout() {
                 { Number(product.quantity * product.price)
                   .toFixed(2).split('.').join(',') }
               </div>
-              <div
-                key={ i }
+              <button
+                type="button"
+                onClick={ remove }
+                // key={ i }
                 data-testid={ `customer_checkout__element-order-table-remove-${i}` }
               >
-                [REMOVE]
-              </div>
+                Remover
+              </button>
             </div>
           // } componente?
           ))
@@ -70,16 +81,19 @@ export default function Checkout() {
         }
       </span>
 
-      <select data-testid="customer_checkout__select-seller">
-        <option value="seller">seller</option>
-      </select>
+      <div>
 
-      <input type="text" data-testid="customer_checkout__input-address" />
-      <input type="text" data-testid="customer_checkout__input-address-number" />
+        <select data-testid="customer_checkout__select-seller">
+          <option value="seller">seller</option>
+        </select>
 
-      <button type="button" data-testid="customer_checkout__button-submit-order">
-        Finalizar
-      </button>
+        <input type="text" data-testid="customer_checkout__input-address" />
+        <input type="text" data-testid="customer_checkout__input-address-number" />
+
+        <button type="button" data-testid="customer_checkout__button-submit-order">
+          Finalizar
+        </button>
+      </div>
 
     </main>
   );
