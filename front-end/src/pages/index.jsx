e9c510c87a9b62175cad8497c62105987bafd4a0
willian-macedo <willian.macedo@gmail.com>
2023-03-08 20:31:02 -0300

    fix lint

--- front-end/src/pages/index.jsx
+++ front-end/src/pages/index.jsx
@@ -2,22 +2,12 @@ import Login from './login';
 import Register from './register';
 import Products from './customer/products';
 import Checkout from './customer/checkout';
-<<<<<<< HEAD
-import Order from './customer/orders';
-import OrderDetails from './customer/orderDetails';
-=======
 import Orders from './customer/orders';
->>>>>>> origin/main-group-18-customer-orders
 
 export {
   Login,
   Register,
   Products,
   Checkout,
-<<<<<<< HEAD
-  Order,
-  OrderDetails,
-=======
   Orders,
->>>>>>> origin/main-group-18-customer-orders
 };
